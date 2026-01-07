f6c3e8f3b45f43f9e0e212e9d913e7ca0fc70abf
arthur-adjedj <arthur.adjedj@ens-paris-saclay.fr>
2026-01-07 16:48:31 +0100

    prout

--- Misc/Graphviz.lean
+++ Misc/Graphviz.lean
@@ -8,7 +8,9 @@ open ProofWidgets ProofWidgets.HtmlEval ProofWidgets.HtmlCommand
 open Lean.Server.RpcEncodable (rpcEncode)
 
 def mkHtmlDotStx (ref : Syntax) (toDot : Std.Format) : Elab.Command.CommandElabM Unit := do
-  let html ← `(term|<embed type="text/html" src={"https://quickchart.io/graphviz?graph=" ++ $(Syntax.mkStrLit toDot.pretty)}> </embed>)
+  let url := "https://quickchart.io/graphviz?graph=" ++ (toDot.pretty /-|>.replace '\n' "" |>.replace ' ' ""-/)
+  logInfo s!"url : {url}"
+  let html ← `(term|<embed type="text/html" src={$(Syntax.mkStrLit url)}> </embed>)
   let htX ← Elab.Command.liftTermElabM <| evalCommandMHtml <| ← ``(ProofWidgets.HtmlEval.eval $html)
   let ht ← htX
   liftCoreM <| Widget.savePanelWidgetInfo
