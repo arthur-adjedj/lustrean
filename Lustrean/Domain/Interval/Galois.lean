e80d173a1663e6dca021230ec90210c0a8d8666f
ayhon <ayhonnick@gmail.com>
2026-02-05 14:25:21 +0100

    refactor: change notation of `Set.min?` and `Set.max?`

--- Lustrean/Domain/Interval/Galois.lean
+++ Lustrean/Domain/Interval/Galois.lean
@@ -37,7 +37,7 @@ def min?{α: Type}[PartialOrder α](s: Set α)(_nonempty: s ≠ ∅ ): WithBot �
 -- May be worth looking into once the proofs are done, or if they become
 -- more complicated than expected.
 
-notation "⨅₂" s:max => Set.min? s (by grind)
+notation "⨅₀" s:max => Set.min? s (by grind)
 
 open Classical in
 noncomputable
@@ -47,9 +47,9 @@ def max?{α: Type}[PartialOrder α](s: Set α)(_nonempty: s ≠ ∅): WithTop α
   else
     ⊤
 
-notation "⨆₂" s:max => Set.max? s (by grind)
+notation "⨆₀" s:max => Set.max? s (by grind)
 
-theorem min?_hle_max?(s: Set Int)(h: s ≠ ∅): ⨅₂ s ≤∘ ⨆₂ s
+theorem min?_hle_max?(s: Set Int)(h: s ≠ ∅): ⨅₀ s ≤∘ ⨆₀ s
 := by
   dsimp only [Set.min?, Set.max?]
   split
@@ -64,7 +64,7 @@ theorem min?_hle_max?(s: Set Int)(h: s ≠ ∅): ⨅₂ s ≤∘ ⨆₂ s
   case isFalse =>
     apply WithBot.bot_hle
 
-theorem min?_le_of_mem{s: Set Int}(h: s ≠ ∅): ∀ x ∈ s, ⨅₂ s ≤ x
+theorem min?_le_of_mem{s: Set Int}(h: s ≠ ∅): ∀ x ∈ s, ⨅₀ s ≤ x
 := by
   intros x x_S
   dsimp only [Set.min?]
@@ -75,7 +75,7 @@ theorem min?_le_of_mem{s: Set Int}(h: s ≠ ∅): ∀ x ∈ s, ⨅₂ s ≤ x
     apply bound x_S
   case isFalse => apply bot_le
 
-theorem le_max?_of_mem(s: Set Int)(h: s ≠ ∅): ∀ x ∈ s, x ≤ ⨆₂ s
+theorem le_max?_of_mem(s: Set Int)(h: s ≠ ∅): ∀ x ∈ s, x ≤ ⨆₀ s
 := by
   intros x x_S
   dsimp only [Set.max?]
@@ -87,13 +87,13 @@ theorem le_max?_of_mem(s: Set Int)(h: s ≠ ∅): ∀ x ∈ s, x ≤ ⨆₂ s
   case isFalse => apply le_top
 
 theorem le_min?(s: Set Int)(nonempty: s ≠ ∅)
-: ∀ (x: Int), x ≤ ⨅₂ s ↔ (∀ z ∈ s, x ≤ z)
+: ∀ (x: Int), x ≤ ⨅₀ s ↔ (∀ z ∈ s, x ≤ z)
 := by
   intros x
   constructor <;> intros hyp
   · intros z z_s
     apply WithBot.coe_le_coe.mp
-    calc x ≤ ⨅₂ s := by assumption
+    calc x ≤ ⨅₀ s := by assumption
          _ ≤ z := by apply s.min?_le_of_mem <;> assumption
   · have bd: BddBelow s := by
       simp only [BddBelow,lowerBounds,Set.Nonempty,Set.mem_setOf_eq]
@@ -106,13 +106,13 @@ theorem le_min?(s: Set Int)(nonempty: s ≠ ∅)
     assumption
 
 theorem max?_le(s: Set Int)(nonempty: s ≠ ∅)
-: ∀ (x: Int), ⨆₂ s ≤ x ↔ (∀ z ∈ s, z ≤ x)
+: ∀ (x: Int), ⨆₀ s ≤ x ↔ (∀ z ∈ s, z ≤ x)
 := by
   intros x
   constructor <;> intros hyp
   · intros z z_s
     apply WithTop.coe_le_coe.mp
-    calc z ≤ ⨆₂ s := by apply s.le_max?_of_mem <;> assumption
+    calc z ≤ ⨆₀ s := by apply s.le_max?_of_mem <;> assumption
          _ ≤ x := by assumption
   · have bd: BddAbove s := by
       simp only [BddAbove,upperBounds,Set.Nonempty,Set.mem_setOf_eq]
@@ -124,7 +124,7 @@ theorem max?_le(s: Set Int)(nonempty: s ≠ ∅)
     have ⟨_, _⟩ := Classical.choose_spec h
     assumption
 
-theorem le_min?_of_mem(s: Set Int)(h: s ≠ ∅): ∀ x ∈ s, x = ⨅₂ s ↔ x ≤ ⨅₂ s
+theorem le_min?_of_mem(s: Set Int)(h: s ≠ ∅): ∀ x ∈ s, x = ⨅₀ s ↔ x ≤ ⨅₀ s
 := by
   intros x x_S
   dsimp only [Set.min?]
@@ -134,7 +134,7 @@ theorem le_min?_of_mem(s: Set Int)(h: s ≠ ∅): ∀ x ∈ s, x = ⨅₂ s ↔ 
   specialize min_lb x_S
   grind only
 
-theorem max?_le_of_mem(s: Set Int)(h: s ≠ ∅): ∀ x ∈ s, x = ⨆₂ s ↔ ⨆₂ s ≤ x
+theorem max?_le_of_mem(s: Set Int)(h: s ≠ ∅): ∀ x ∈ s, x = ⨆₀ s ↔ ⨆₀ s ≤ x
 := by
   intros x x_S
   dsimp only [Set.max?]
@@ -145,25 +145,25 @@ theorem max?_le_of_mem(s: Set Int)(h: s ≠ ∅): ∀ x ∈ s, x = ⨆₂ s ↔ 
   grind only
 
 theorem bdd_of_le_min?(s: Set Int)(h: s ≠ ∅)(e: Int)
-: e ≤ (⨅₂ s) → BddBelow s
+: e ≤ (⨅₀ s) → BddBelow s
 := by
   intros hyp
   simp only [BddBelow,lowerBounds,Set.Nonempty,Set.mem_setOf_eq]
   exists e
   intros x x_s
   apply WithBot.coe_le_coe.mp
-  calc e ≤ ⨅₂ s := by assumption
+  calc e ≤ ⨅₀ s := by assumption
        _ ≤ x    := by apply Set.min?_le_of_mem <;> assumption
 
 theorem bdd_of_max?_le(s: Set Int)(h: s ≠ ∅)(e: Int)
-: (⨆₂ s) ≤ e → BddAbove s
+: (⨆₀ s) ≤ e → BddAbove s
 := by
   intros hyp
   simp only [BddAbove,upperBounds,Set.Nonempty,Set.mem_setOf_eq]
   exists e
   intros x x_s
   apply WithTop.coe_le_coe.mp
-  calc x ≤ ⨆₂ s := by apply Set.le_max?_of_mem <;> assumption
+  calc x ≤ ⨆₀ s := by apply Set.le_max?_of_mem <;> assumption
        _ ≤ e    := by assumption
 
 end Set
@@ -182,7 +182,7 @@ def abstract(s: Set Int): Interval :=
   if cond: s = ∅ then
     ⊥
   else
-    mk (⨅₂ s) (⨆₂ s) (s.min?_hle_max? cond)
+    mk (⨅₀ s) (⨆₀ s) (s.min?_hle_max? cond)
 
 attribute [simp] WithBot.coe_le_coe WithTop.coe_le_coe
 
@@ -205,9 +205,9 @@ def gc: GaloisConnection abstract concrete := by
     · have ⟨xl_minS, maxS_xh⟩ := WithBot.coe_le_coe.mp hyp
       intros e e_S
       constructor
-      · calc xl ≤ ⨅₂ s := by assumption
+      · calc xl ≤ ⨅₀ s := by assumption
               _ ≤ e   := by apply Set.min?_le_of_mem <;> assumption
-      · calc e ≤  ⨆₂ s := by apply Set.le_max?_of_mem <;> assumption
+      · calc e ≤  ⨆₀ s := by apply Set.le_max?_of_mem <;> assumption
               _ ≤ xh  := by assumption
     · apply WithBot.coe_le_coe.mpr
       simp only [NonEmpty.mk_le_mk]
@@ -215,13 +215,13 @@ def gc: GaloisConnection abstract concrete := by
       · match xl with
         | ⊥ => apply bot_le
         | (xl: Int) =>
-          have ⟨l, l_S, l_eq⟩ : ∃ l ∈ s, l = ⨅₂ s := by
-            fun_cases (⨅₂ s) <;> rename_i h
+          have ⟨l, l_S, l_eq⟩ : ∃ l ∈ s, l = ⨅₀ s := by
+            fun_cases (⨅₀ s) <;> rename_i h
             · have ⟨e, h⟩ := Classical.choose_spec h
               simp only [WithBot.coe_inj, exists_eq_right, e]
             · exfalso
               rename_i h
-              have : xl ≤ ⨅₂ s := by
+              have : xl ≤ ⨅₀ s := by
                 apply Set.le_min? s nonempty xl |>.mpr
                 intros z z_s
                 have ⟨xl_z, z_xh⟩ := hyp z_s
@@ -234,13 +234,13 @@ def gc: GaloisConnection abstract concrete := by
       · match xh with
         | ⊤ => apply le_top
         | (xh: Int) =>
-          have ⟨l, l_S, l_eq⟩ : ∃ l ∈ s, l = ⨆₂ s := by
-            fun_cases (⨆₂ s) <;> rename_i h
+          have ⟨l, l_S, l_eq⟩ : ∃ l ∈ s, l = ⨆₀ s := by
+            fun_cases (⨆₀ s) <;> rename_i h
             · have ⟨e, h⟩ := Classical.choose_spec h
               simp only [WithTop.coe_inj, exists_eq_right, e]
             · exfalso
               rename_i h
-              have : ⨆₂ s ≤ xh := by
+              have : ⨆₀ s ≤ xh := by
                 apply Set.max?_le s nonempty xh |>.mpr
                 intros z z_s
                 have ⟨xl_z, z_xh⟩ := hyp z_s
