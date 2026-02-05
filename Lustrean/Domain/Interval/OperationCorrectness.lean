4f1a29045a96dc7a1e3d8ca747b446af1e3cb23b
ayhon <ayhonnick@gmail.com>
2026-02-05 14:49:31 +0100

    feat: add randomness correctness

--- Lustrean/Domain/Interval/OperationCorrectness.lean
+++ Lustrean/Domain/Interval/OperationCorrectness.lean
@@ -4,6 +4,8 @@ import Lustrean.Domain.Interval.ValueDomain -- Value domain operations
 import Lustrean.Domain.GaloisConnection -- Extra definitions on Galois connections
 import Mathlib.Algebra.Group.Pointwise.Set.Basic -- Set operations
 
+attribute [-simp] bot_eq_zero' bot_eq_one' «Prop».bot_eq_false
+
 namespace Lustrean.Interval
 
 section OperationCorrectness
@@ -39,5 +41,22 @@ theorem refine_correct (ord : CompareOp) (x y: Interval)
                                   ∧ (∃ e' ∈ y.concrete, ord.toProp e e') }
 := by
   sorry
+
+theorem rand_correctness (l? r?: Option Int)
+: { e | (∀ l ∈ l?, l ≤ e) ∧ (∀ r ∈ r?, e ≤ r)} ⊆ (rand l? r?).concrete
+:= by
+  simp only [Option.mem_def, concrete]
+  fun_cases (rand l? r?)
+  <;> intros e e_l
+  · simp only [Option.some.injEq, forall_eq', Set.mem_setOf_eq] at e_l
+    rename_i x y
+    have: x ≤ y := by cases e_l; trans <;> assumption
+    simp only [ofPair, this, WithBot.coe_hle_coe, reduceDIte, Set.mem_setOf_eq, WithBot.coe_le_coe, WithTop.coe_le_coe, e_l]
+    trivial
+  · simpa only [bot_le, WithTop.coe_le_coe, true_and, Set.mem_setOf_eq, reduceCtorEq,
+    IsEmpty.forall_iff, implies_true, Option.some.injEq, forall_eq'] using e_l
+  · simpa only [WithBot.coe_le_coe, le_top, and_true, Set.mem_setOf_eq, Option.some.injEq,
+    forall_eq', reduceCtorEq, IsEmpty.forall_iff, implies_true] using e_l
+  · simp only [bot_le, le_top, and_self, Set.setOf_true, Set.mem_univ]
 
 end OperationCorrectness
